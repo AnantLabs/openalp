a1065e6df057ddf7617b0076028f3a9de4438b31
scarr.adam <scarr.adam@70d421f4-045c-11de-9a48-596ebbf2fbe4>
2009-03-24 03:05:59 +0000

    Few bug fixes.

---
+++ trunk/src/Test/SimpleGrammarTests.java
@@ -0,0 +1,39 @@
+package Test;
+
+import Core.Lexicon;
+import Core.Grammar;
+import Core.FileParser;
+
+/**
+ * This file is part of OpenALP.
+ * <p/>
+ * OpenALP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * OpenALP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with OpenALP.  If not, see <a href='http://www.gnu.org/licenses/'>http://www.gnu.org/licenses/</a>.
+ * <p/>
+ *
+ * Runs through a few simple tests to validate the core logic in grammar.
+ *
+ * @author Adam Scarr
+ * @since r1
+ */
+public class SimpleGrammarTests {
+    public static void main(String[] args) {
+        Lexicon lexicon = new Lexicon();
+        Grammar grammar = new Grammar(lexicon);
+        FileParser fp = new FileParser(grammar);
+
+        fp.testFile("data/simple.tst");
+        grammar.clear();
+        fp.testFile("data/reverse.tst");
+    }
+}
